159d47e06aa2e58aa8491cffbb454a3106457c2b
AlohomoraPZX <111486670+AlohomoraPZX@users.noreply.github.com>
2022-09-07 20:22:02 +0800

    Only for replacement. Still a concept.

---
+++ JudgeClient.cs
@@ -0,0 +1,9 @@
+using System;
+namespace JSMSOJ_Judge {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("This is still remains a concept.");
+			Console.ReadKey();
+		}
+	}
+}
